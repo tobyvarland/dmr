4be9767a31815912b1e7b8b09a303a266d64a986
Toby Varland <toby.varland@varland.com>
2021-04-20 10:16:47 -0400

    Update formatting of DMR show page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -32,9 +32,9 @@ module ApplicationHelper
   def card_list(title, items)
     list_items = []
     items.each do |item|
-      label = content_tag(:div, "#{item[:label]}:", class: ["w-33", "text-uncolor", "text-small-baseline", "text-nowrap"])
-      value = content_tag(:div, item[:value].html_safe)
-      list_items << content_tag(:li, label + value, class: ["list-group-item", "d-flex", "flex-row", "align-items-top", "justify-content-start", "bg-light"])
+      label = content_tag(:div, "#{item[:label]}:", class: ["text-uncolor", "small", "text-nowrap", "lh-1"])
+      value = content_tag(:div, item[:value].html_safe, class: ["lh-sm", "my-0"])
+      list_items << content_tag(:li, label + value, class: ["list-group-item", "d-flex", "flex-column", "align-items-top", "justify-content-start", "bg-light"])
     end
     header = content_tag(:h6, title, class: ["card-header"])
     list = content_tag(:ul, list_items.join.html_safe, class: ["list-group", "list-group-flush"])
